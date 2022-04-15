1ca499293bbd84392e3f7d0c7329b1b74283c3a6
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-04-14 20:05:52 -0700

    ts revert

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Logo} from './Logo.tsx';
+import Logo from './Logo';
 import { FaGithub } from 'react-icons/fa';
 import { useState } from 'react';
 const nav_items = [
