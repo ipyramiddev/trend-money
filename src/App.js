463523f2f2ab27d650c20d5e6119205edb7db0ec
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-05-15 22:04:34 -0700

    main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from "react";
-import { ethers } from "ethers";
+import { ethers, BigNumber } from "ethers";
 // import PiePool from './components/pool/PiePool.js';
 import SeamPool from "./components/pool/SeamPool.js";
 import pool_data from './pool_data.js';
@@ -7,6 +7,7 @@ import PvChart from "./components/graphs/LineChart.js";
 import UserHeader from "./components/UserHeader.js";
 import {getPool} from "./hooks/useUni.js";
 import UniPool from "./components/pool/UniPool.js";
+import SubSeam from "./components/pool/SubSeam.js";
 function App() { 
 
   const [poolData, setPoolData] = useState(null);
@@ -22,21 +23,27 @@ function App() {
     const balance = await provider.getBalance(accounts[0]);
     const balanceInEther = ethers.utils.formatEther(balance);
     const block = await provider.getBlockNumber();
+    // const pool = await getPool(provider);
     setWalletConnected(true);
     setProvider(provider);
     setWalletAddress(accounts[0]);
     // getPool(provider).then(data => {
     //   console.log("pool data",data);z
-    //   setPoolData(data);
+      // setPoolData(pool);
       
     // });
     setWalletBalance(balanceInEther);
+    
     }
   }
 
-  const loadPoolData = async () => {
+  // const loadBalances = async () => {
+  //   if (walletConnected) {
+
+  const loadPoolData = async (pool_address) => {
     // if (provider) {
-      const data = await getPool(provider);
+      const data = await getPool(provider, pool_address);
+      
       setPoolData(data);
     // }
   }
@@ -48,12 +55,15 @@ function App() {
   return (
     <div className="px-10 py-4 h-full bg-black text-white">
       <div className="flex flex-col items-center ">
-        <button onClick={connectToMetamask} className="rounded-lg w-70 font-2xl p-3 m-5 border-dashed border-white border-2 hover:bg-white hover:text-blac" >Connect Wallet</button>
-        {walletConnected?null:"Not Connected"}
+        
+        {walletConnected?null:(<button onClick={connectToMetamask} className="rounded-lg w-70 font-2xl p-3 m-5 border-dashed border-white border-2 hover:bg-white hover:text-blac" >Connect Wallet</button>)}
         {/* {walletAddress} */}
         
         {walletConnected? (<button onClick={loadPoolData} className="rounded-lg w-70 font-2xl p-3 m-5 border-dashed border-white border-2 hover:bg-white hover:text-blac" >Get Pools</button>) : null}
-        {poolData!=null ? <UniPool poolData={poolData} /> : null}
+        {poolData!=null ? (<div>
+          
+          <p>{poolData.total}</p>
+        </div>) : null}
         <UserHeader walletAddress={walletAddress} walletBalance={walletBalance} />
     {/* <p>Connection Status: {walletConnected? (<p className="text-green">"Connected"</p>) : (<p className="opacity-40 text-center">Not Connected</p>)}</p> */}
             
@@ -62,6 +72,7 @@ function App() {
       <div className="flex-cols-2 ">
         ( <SeamPool {...pool_data.YETF} />)
               {/* <SeamPool  pool={poolData}/> */}
+
               
           </div>
         
