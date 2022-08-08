ab300b5b1734e0307ba4d82a357d26d3b317fb2f
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-08-08 09:54:12 -0700

    explorer

--- src/hooks/useAptos.ts
+++ src/hooks/useAptos.ts
@@ -1,18 +1,12 @@
-import { AptosClient, AptosAccount, FaucetClient, BCS, Types, TxnBuilderTypes,HexString } from "aptos";
-import { AccountResource } from "aptos/dist/api/data-contracts";
+import { AptosClient, AptosAccount, FaucetClient, BCS, Types, TxnBuilderTypes, HexString, TokenClient } from "aptos";
+// import { AccountResource } from "aptos/dist/api/data-contracts";
+import { BaseContract } from "ethers";
 const NODE_URL = "https://fullnode.devnet.aptoslabs.com";
 const FAUCET_URL = "https://faucet.devnet.aptoslabs.com";
 
 const client = new AptosClient(NODE_URL);
 const faucetClient = new FaucetClient(NODE_URL, FAUCET_URL);
-// const tempAccount = new AptosAccount({address: "0x0000000000000000000000000000000000000000", privateKey: "0x0000000000000000000000000000000000000000000000000000000000000000"});
-
-// export const connectWallet = async () => {
-//     console.log("Connecting wallet...");
-// }
-
-// const resourceMap = {
-
+const tokenClient = new TokenClient(client);
 
 
 export const useFaucet = async (account: AptosAccount) => {
@@ -22,9 +16,8 @@ export const useFaucet = async (account: AptosAccount) => {
 }
 
 
-
-export const loadCoins = async () => {
-    const resources  = await client.getAccountResources(new HexString("0x1d40175352316901bb8306b29a919da75f8b305f9bb9fa265f308c67cb409270"));
+export const loadCoins = async (account:string) => {
+    const resources  = await client.getAccountResources(new HexString(account));
     const accountResource = resources.find((r) => r.type === "0x1::coin::CoinStore<0x1::aptos_coin::AptosCoin>");
     // const coins = accountResource.coins;
     // const coins = (accountResource?.data as any)
@@ -35,12 +28,16 @@ export const loadCoins = async () => {
 
 export const loadCoinStore = async () => {
     const resources  = await client.getAccountResources(new HexString("0x1d40175352316901bb8306b29a919da75f8b305f9bb9fa265f308c67cb409270"));
-    const coinStore = resources[0] as AccountResource;
-    const coinEvents = resources[1] as AccountResource;
-    const coin_txs_count = (coinEvents?.data as any).register_events as {counter: number};
-    // console.log(coinStore.data.coin.value);
-    const deposit_count = ((coinStore?.data as any).deposit_events as any).counter as number;
-    const withdraw_count = ((coinStore?.data as any).withdraw_events as any).counter as number;
+
+    
+    // const coinEvents = resources[1] as Types.AccountResource;
+    const aptCoin = resources[0] as Types.AccountResource;
+    const coinEvents = resources.find((r) => (r.type as string)==="0x1::coin::CoinEvents") as Types.AccountResource;
+
+    const coin_txs_count = ((coinEvents.data as any).register_events as any).counter as number;
+
+    const deposit_count =0// ((aptCoin.data as any).deposit_events as any).counter as number;
+    const withdraw_count =0// ((aptCoin.data?.withdraw_events as any).counter as number;
     const balance = (resources[0].data as any).coin.value;
     const coins = {
         coins: [],
@@ -50,22 +47,24 @@ export const loadCoinStore = async () => {
         coin_txs_count,
         coin_txs:[]
     }
-    console.log(coins);
+    console.log("COINS",coins);
     return coins
 }
 
 
 export const loadNfts = async (address:string) => {
-    const collections =[] as any[];
-    const resources = await client.getAccountResources("0x1d40175352316901bb8306b29a919da75f8b305f9bb9fa265f308c67cb409270");
-    console.log(resources);
-    const collection_resources = resources[4].data as any;
-    const collection_count = (collection_resources.create_collection_events as any).counter as number;
-    const minted_count = (collection_resources.create_token_events as any).counter as number;
+    
+    const resources = await client.getAccountResources(address) as Types.AccountResource[];
+    console.log("NFT Resources: ", resources);
+    const collections = (resources.find((r:Types.AccountResource) => r.type === "0x3::token::Collections")) as Types.AccountResource;
+    
+    const minted_count = ((collections.data as any).create_token_data_events).counter as number;
+    const collection_count = ((collections.data as any).create_collection_events as any).counter as number;
+    const collection_data = (collections.data as any).collections as any[];
     console.log(`Collection count: ${collection_count}`);
-    console.log(collection_resources);
+    // console.log(collection_resources);
     return  {
-        collections: [],
+        collections: collection_data,
         collection_count: collection_count,
         minted_count: minted_count,
         received_count: 0,
@@ -73,17 +72,18 @@ export const loadNfts = async (address:string) => {
 }
 }
 
-export const sendTransaction= async () =>{
+export const sendTransaction= async (toAddr:string) =>{
 // Generate a transaction
-const response = await window.martian.connect();
-const sender = response.address;
+// const response = await window.martian.connect();
+// const sender = response.address;
 const payload = {
     type: "script_function_payload",
     function: "0x1::coin::transfer",
     type_arguments: ["0x1::aptos_coin::AptosCoin"],
-    arguments: ["0x96da8990a7230a82250e85d943ca95e2e9319e5558b0f544f2d7a6aad327e46f", "50"]
+    arguments: [toAddr, "5000"]
 };
-const transactionRequest = await window.aptos.generateTransaction(sender, payload);
+const transactionRequest = await window.martian.generateTransaction("0x1d40175352316901bb8306b29a919da75f8b305f9bb9fa265f308c67cb409270", payload);
+const txnHash = await window.martian.signAndSubmitTransaction(transactionRequest);
 console.log(transactionRequest);
 };
 
@@ -92,6 +92,14 @@ export const stringToHex= (text: string) => {
     const encoded = encoder.encode(text);
     return Array.from(encoded, (i) => i.toString(16).padStart(2, "0")).join("");
   }
+
+export const loadModules = async (address: string) => {
+    const modules = await client.getAccountModules(address) as Types.MoveModule[];
+    return modules;
+
+}
+
+
 
 // export const hexToString= (hex: string) => {
 //     const decoder = new TextDecoder();
