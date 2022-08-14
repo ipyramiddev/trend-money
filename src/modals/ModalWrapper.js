0258cc6724d74cb9c14161dff1f91ecf4d56efcb
Jack-kelly-22 <60161371+Jack-kelly-22@users.noreply.github.com>
2022-08-14 14:18:06 -0700

    create txn modal for gen txs

--- src/modals/ModalWrapper.js
+++ src/modals/ModalWrapper.js
@@ -48,17 +48,15 @@ function ModalWrapper(props) {
             transform transition-all 
             "
                         >
-                            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                            <div className="bg-black seam-outlinw text-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                                 <div className="sm:flex sm:items-center">
 
                                     <div className="mt-3 text-center">
-                                        <p className="text-2xl font-medium ">
+                                        {/* <p className="text-2xl font-medium ">
                                             {props.title}
-                                        </p>
+                                        </p> */}
                                         <div className="mt-2">
-                                            <p className="text-sm text-gray-500">
-                                                some description
-                                            </p>
+                                            
                                             <div>
                                                 {props.children}
                                                 <button
--- src/modals/TransactionModal.tsx
+++ src/modals/TransactionModal.tsx
@@ -1,37 +1,42 @@
-import { AptosAccount, BCS, TxnBuilderTypes } from "aptos";
-import { MoveFunction, MoveModule, MoveModuleBytecode, MoveType } from "aptos/dist/generated";
+import { AptosAccount, AptosClient, BCS, TxnBuilderTypes } from "aptos";
+import { MoveFunction, MoveModule, MoveModuleBytecode, MoveType, MoveValue, TransactionPayload } from "aptos/dist/generated";
 // import { MoveFunction, MoveModuleABI, MoveTypeId, AccountResource } from "aptos/dist/api/data-contracts";
 import { TransactionPayloadScriptFunction } from "aptos/dist/transaction_builder/aptos_types";
+import TxnHeader from "components/txn/TxnHeader";
 import { useEffect, useRef, useState } from "react";
+import { generic_serialize } from "util/aptosUtils";
 import ModalWrapper from "./ModalWrapper";
 
 interface txnModalProps {
-    sender: string;
-    client: AptosAccount;
-    address: string
+    client: AptosClient;
+    address: string,
+    sender: string,
     func: MoveFunction;
-    module: MoveModule;
-    params: any[];
-    args: any[];
-    arg_types: string[];
+    module: MoveModuleBytecode;
+    // module_name: string;
+    
+    // args?: any[];
+    // arg_types?: string[];
     isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void;
 
 }
 
 
 
-const createPayload = (sender: string, address:string, client: AptosAccount, module: MoveModule, func: MoveFunction, params: any[], args: any[], arg_types: string[]) => {
+const createPayload = (address:string, client: AptosClient, module: MoveModuleBytecode, func: MoveFunction, params: any[], args: any[], arg_types: string[]) => {
     const serializedArgs = args.map((param:MoveType, index) => {
-        // return generic_serialize(param, arg_types, args[index]);
-        {}
+        const arg = args[index];
+        return generic_serialize(arg);
+        // {}
     }) as any[]
 
     const scriptFunctionPayload =  new TxnBuilderTypes.TransactionPayloadScriptFunction(
       TxnBuilderTypes.ScriptFunction.natural(
-        module.name,
+        address+'::'+module.abi?.name,
         func.name,
         [],
-        [
+        [ 
           // B(name),
           // BCS.bcsSerializeStCS.bcsSerializeStrr(description),
           // BCS.bcsSerializeStr(uri),
@@ -44,31 +49,79 @@ const createPayload = (sender: string, address:string, client: AptosAccount, mod
   }
 
 
-const TransactionModal = ({isOpen,sender,client, address, func, params,args,arg_types}: txnModalProps) =>{
+const TransactionModal = ({isOpen,sender,client, address,module,setIsOpen, func}: txnModalProps) =>{
     const [open, setOpen] = useState(isOpen);
+    const [argList, setArgList] = useState<any[]>([]);
     const cancelButtonRef = useRef(null);
-    const [payload, setPayload] = useState<TransactionPayloadScriptFunction | undefined>();
-    // const [
-    // useEffect(() => {
-    //     if (open) {
-    //         // setPayload(createPayload(sender, address, client, func, params, args, arg_types));
-    //     }
-    // } , [open]);
+    
+    const params = func.params as MoveType[]
+    const module_name = module.abi?.name;
+    
+
+    const updateArg = (index: number, value: string) => {
+        const newArgs = [...argList];
+        newArgs[index] = value;
+        setArgList(newArgs);
+    }
+
+    const verifyArgs = () => {
+      console.log(argList);
+    }
+
+    const sendTxn = async () => {
+        // const payload = createPayload(address, client, module, func, params, argList, []);
+        const payload = {
+          type: "script_function_payload",
+          function: (address+'::'+module.abi?.name+'::'+func.name) as string,
+          // type_arguments: ["0x1:string:String"],
+          arguments: [generic_serialize("d")] as any[]
+      } as TransactionPayload;
+      console.log("Payload",payload);
+        const [{ sequence_number: sequenceNumber }, chainId] = await Promise.all([
+          client.getAccount("0x1d40175352316901bb8306b29a919da75f8b305f9bb9fa265f308c67cb409270"),
+          client.getChainId(),
+        ]);
+      
+        // const rawTxn = new TxnBuilderTypes.RawTransaction(
+        //   TxnBuilderTypes.AccountAddress.fromHex("0x1d40175352316901bb8306b29a919da75f8b305f9bb9fa265f308c67cb409270"),
+        //   BigInt(sequenceNumber),
+        //   payload,
+        //   BigInt(1000),
+        //   BigInt(1),
+        //   BigInt(Math.floor(Date.now() / 1000) + 10),
+        //   new TxnBuilderTypes.ChainId(chainId),
+        // );
+        const transactionRequest = await window.martian.generateTransaction("0x1d40175352316901bb8306b29a919da75f8b305f9bb9fa265f308c67cb409270", payload);
+const txnHash = await window.martian.signAndSubmitTransaction(transactionRequest);
+      
+        // const bcsTxn = AptosClient.generateBCSTransaction(account, rawTxn);
+        // const pendingTxn = await client.submitSignedBCSTransaction(bcsTxn);
+        // await client.waitForTransaction(pendingTxn.hash);
+
+    }
 
     return (
-      <ModalWrapper open={open} setOpen={setOpen} cancelButtonRef={cancelButtonRef} title="tets">
+      <ModalWrapper open={isOpen} setOpen={setIsOpen} cancelButtonRef={cancelButtonRef} title="tets">
   
-        {/* show seam pool name */}
-        {/* DEPOSIT COIN(S) DEPOSIT AMOUNTs */}
-  
-        {/* small boxes each sub-pool  */}
-        <p>content</p>
+
+        {/* show input for each of the params and store them in argList */}
+        <TxnHeader func_name={func.name} module_name={module_name||"err"} address={address}/>
+        {params.map((param:MoveType, index) => {
+            return (
+              <span key={index} className="flex flex-row outline outline-2 outline-white">
+                <p>{param}</p>
+                <input className="bg-black" type="text" onChange={(event)=>updateArg(index,event.target.value)}/>
+              </span>
+            )
+        } )}
+        <button className="seam-button" onClick={()=>verifyArgs()}>verify params</button>
+
   
         {/* APPROVE SPEND Button */}
         
   
         {/* DEPOSIT button */}
-  
+        <button className="seam-button" onClick={()=>sendTxn()}>Send Txn</button>
       </ModalWrapper>
     );
   }
