f3d8149858e5049fb8640561cf370edea1392e67
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-08-29 09:54:27 -0700

    add module fixes

--- src/sections/modules/ModuleTypes.tsx
+++ src/sections/modules/ModuleTypes.tsx
@@ -4,33 +4,49 @@ import { formatParam } from "hooks/formatting";
 const ModuleTypes = ({ module }: { module: Types.MoveModuleBytecode }) => {
     const { abi } = module;
     return (
-        <div className="flex flex-col"><p className="text-center text-2xl p-2">Module Types(structs)</p>
-            <div className="flex flex-row items-center scrollbar dappScroll ">
-                {abi?.structs.map((struct: Types.MoveStruct) => {
-                    return (
-                        <div className="outline w -full rounded-xl m-2 px-2">
-                            <div className="flex flex-row gap gap-1">
-                                <h3 className="text-2xl">{struct.name}</h3>
-                                <p className="label">:struct</p>
-                            </div>
-                            <div className="p-2">
-                                {struct.fields.map((field: any) => {
-                                    return (
-                                        <div className="flex p-1 gap-1 justify-end">
-                                            <p>{formatParam(field.name)}:</p>
-                                            <p>{field.type}</p>
-                                        </div>
-                                    )
-                                }
-                                )}
-                            </div>
-                        </div>
-                    )
-                })}
-
-            </div>
+        <div>
+        <p className="text-center text-2xl p-2">Module Types(structs)</p>
+        <div className="flex flex-col scrollbar w-60 scrollbar-thumb-blue2 dappScroll">
+            {ModuleStructs(module)}
+        </div>
         </div>
     );
 }
 
+
+const ModuleStructs = (module:Types.MoveModuleBytecode) => {
+    const {abi } = module;
+    return (
+        <div className="flex flex-row items-center   ">
+                {abi?.structs.map((struct: Types.MoveStruct) => {
+                    return ModuleStruct(struct);
+                })}
+
+            </div>
+    )
+}
+
+
+const ModuleStruct = (struct:Types.MoveStruct)=>{
+    return (
+        <div className="outline w-full rounded-xl m-2 px-2">
+            <div className="flex flex-row gap gap-1">
+                <h3 className="text-2xl">{struct.name}</h3>
+                <p className="label">:struct</p>
+            </div>
+            <div className="p-2">
+                {struct.fields.map((field: any) => {
+                    return (
+                        <div className="flex p-1 gap-1 justify-end">
+                            <p>{formatParam(field.name)}:</p>
+                            <p>{field.type}</p>
+                        </div>
+                    )
+                }
+                )}
+            </div>
+        </div>
+    )
+
+}
 export default ModuleTypes;
--- src/sections/modules/TxnPreview.tsx
+++ src/sections/modules/TxnPreview.tsx
@@ -1,7 +1,8 @@
 import { AptosAccount, AptosClient } from "aptos";
-import { MoveFunction, MoveFunctionGenericTypeParam, MoveModuleBytecode } from "aptos/dist/generated";
+import { MoveFunction, MoveFunctionGenericTypeParam, MoveModuleBytecode, MoveType } from "aptos/dist/generated";
 import { formatParam } from "hooks/formatting";
 import TransactionModal from "modals/TransactionModal";
+import { useState } from "react";
 
 interface TxnPreviewProps {
     address: string;
@@ -13,10 +14,18 @@ interface TxnPreviewProps {
 }
 
 const TxnPreview = ({address, module, func, params,setShowTxnModal,client } : TxnPreviewProps) => {
+    const [argList, setArgList] = useState<any[]>([]);
+
+    const updateArg = (index: number, value: string) => {
+        const newArgs = [...argList];
+        newArgs[index] = value;
+        setArgList(newArgs);
+    }
+
     return (
-        <div>
-                <p className="text-3xl p-2 " >Use Module</p>
-        <div className="flex items-center opacity-50 seam-outline">
+        <div className="shadow">
+            <p className="text-3xl p-2 " >Use Module</p>
+        <div className=" items-start seam-outline">
             
                 <div className="flex flex-row items-center gap gap-3">
                     <p className="account-outline text-2xl">{formatParam(address)}</p>
@@ -26,6 +35,16 @@ const TxnPreview = ({address, module, func, params,setShowTxnModal,client } : Tx
                     <p className="text-3xl">::</p>
                     <p className="function-outline text-2xl">{func?.name}</p>
                     
+                </div>
+                <div>
+                {params.map((param:MoveType, index:number) => {
+            return (
+              <div key={index} className="flex flex-row items-baseline justify-start px-2 py-3 m-3 rounded-xl text-white">
+                <p className="p-1 text-bold text-right">{param}</p>
+                <input className="px-3 text-black py-2 rounded-xl outline outline-2" type="text" placeholder={""} value={argList[index]} onChange={(event)=>updateArg(index,event.target.value)}/>
+              </div>
+            )
+        } )}
                 </div>
             </div>
                 <button onClick={()=>setShowTxnModal(true)} className="seam-button ">Create Txn</button>
