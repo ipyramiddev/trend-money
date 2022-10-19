0c5e7fc68938ce49832e9ff9559b46b6ef261b7c
Jack-kelly-22 <jacklaxjk@gmail.com>
2022-10-19 10:36:44 -0700

    temp holder stuff

---
+++ src/pages/AptosStats.tsx
@@ -0,0 +1,10 @@
+const AptosStats = () => {
+    return (
+        <div className="h-screen items-center">
+            
+
+        </div>
+    )
+}
+
+export default AptosStats
--- src/pages/DAO.tsx
+++ src/pages/DAO.tsx
@@ -1,17 +1,36 @@
 // create a functional component that returns a div with a className of "dao"
 //  this page will be the dashboard page for the dao
-export const Dao = () => {
+
+const DAO_ADDR = ""
+
+export const Proposals = () =>{
+    return(
+        <div className="">
+
+        </div>
+    );
+}
+
+
+const Dao = () => {
     return (
-        <div className="dao">
-            <p>Seam Dao</p>
-            {/* <DAOOverview />
-            <Proposals /> */}
+        <div className="dao h-screen">
+            <p className="text-4xl shadow-bl ">Seam Dao</p>
+            {/* <DAOOverview /> */}
+            {/* <Proposals /> */}
         </div>
     )
 }
 
 const DAOOverview = () => {
+
+
+
     return(
-        <div></div>
+        <div>
+
+        </div>
     );
 }
+
+export default Dao
