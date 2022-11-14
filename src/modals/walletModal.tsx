5f77135388e0f226f34452bfbb89c2147d0b8bfd
0xdevnet <0xdevnet@gmail.com>
2022-11-14 03:46:44 -0500

    Move testnet/mainnet toggle to the wallet connect modal

--- src/modals/walletModal.tsx
+++ src/modals/walletModal.tsx
@@ -6,10 +6,11 @@ import TxnHeader from "components/txn/TxnHeader";
 import { useEffect, useRef, useState } from "react";
 import { generic_serialize } from "util/aptosUtils";
 import { useWallet } from '@manahippo/aptos-wallet-adapter';
-// import { ConnectWallet, useWeb3 } from "@fewcha/web3-react";
+import { useWeb3 } from "@fewcha/web3-react";
 import ModalWrapper from "./ModalWrapper";
 import copy from "copy-to-clipboard";
 import AccountOutline from "components/etc/AccountOutline";
+import Select from "react-select";
 
 interface walletModalProps {
   isOpen: boolean;
@@ -34,6 +35,8 @@ const WalletModal = ({ isOpen, setIsOpen }: walletModalProps) => {
     signTransaction,
     // network
   } = useWallet();
+
+  const { isConnected, network } = useWeb3();
 
   const onDisconnect = () => {
     disconnect().then(() => {
@@ -64,8 +67,70 @@ const WalletModal = ({ isOpen, setIsOpen }: walletModalProps) => {
     });
   };
 
+  const options = [
+    // { value: 'devnet', label: 'Devnet' },
+    { value: "https://fullnode.mainnet.aptoslabs.com/", label: "Mainnet" },
+    { value: "https://fullnode.devnet.aptoslabs.com/", label: "Devnet" },
+    { value: "https://fullnode.testnet.aptoslabs.com/", label: "Testnet" },
+    // { value: 'local', label: 'Local' }
+  ];
+
+  const selectStyles = {
+    option: (base: any, state: any) => ({
+      ...base,
+      color: state.isFocused ? "black" : "white",
+      background: state.isFocused ? "white" : "black",
+      
+    }),
+    control: (base: any) => ({
+      ...base,
+      background: "#000",
+      color: "#fff",
+      borderStyle: "dashed",
+      borderWidth: "3px",
+      borderColor: "white",
+      boxShadow: "unset"
+    }),
+    menu: (base: any) => ({
+      ...base,
+      borderRadius: 0,
+      marginTop: 0,
+      color: "#fff",
+    }),
+    menuList: (base: any) => ({
+      ...base,
+      padding: 0,
+      background: "#000",
+      borderStyle: "dashed",
+      borderWidth: "3px",
+      borderColor: "white",
+      borderTop: "unset"
+    }),
+    singleValue: (base: any) => ({
+      ...base,
+      color: '#fff'
+    })
+  };
+
+  const ChainInput = () => (
+    <div className="flex flex-col items-center justify-center mb-2">
+      <Select
+        options={options}
+        className="rounded-xl"
+        styles={selectStyles}
+        value={options.find((option) => option.value === network)}
+        defaultValue={{
+          value: "https://fullnode.mainnet.aptoslabs.com/",
+          label: "Mainnet",
+        }}
+      />
+      {/* <p>network:{network.toString()}</p> */}
+    </div>
+  );
+
   return (
     <ModalWrapper open={isOpen} setOpen={setIsOpen} cancelButtonRef={cancelButtonRef} title="tets">
+      <ChainInput />
       {connected && account!=null ? <div className="flex flex-col justify-between"> 
       <AccountOutline addr={account!=null ? account.address?.toString():"not Connected"}/>
       <p className="text-green1 text-center text-lg">Connected</p>
